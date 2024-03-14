af11aec687d75d146239a9617d90b6387f2daddf
Oleg Klimchuk <koi196621@gmail.com>
2024-03-14 17:30:36 +0200

    Getting started with a project using MongoDB.  Added use of repository.  Added entity class java  2

--- src/main/java/com/example/javatechtask/models/repository/ReportRepo.java
+++ src/main/java/com/example/javatechtask/models/repository/ReportRepo.java
@@ -3,7 +3,6 @@ package com.example.javatechtask.models.repository;
 import com.example.javatechtask.models.SalesAndTrafficReport;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 
 public interface ReportRepo extends MongoRepository<SalesAndTrafficReport, String> {
     SalesAndTrafficReport findBySalesAndTrafficByDate_Date(String date);
