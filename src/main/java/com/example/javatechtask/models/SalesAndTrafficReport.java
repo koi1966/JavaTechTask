693493679cd282a7f0266524540c966616a70e96
Oleg Klimchuk <koi196621@gmail.com>
2024-03-14 17:31:24 +0200

    Getting started with a project using MongoDB.  Added use of repository.  Added entity class java  2

--- src/main/java/com/example/javatechtask/models/SalesAndTrafficReport.java
+++ src/main/java/com/example/javatechtask/models/SalesAndTrafficReport.java
@@ -8,8 +8,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 import java.util.UUID;
 
-//@Setter
-//@Getter
+@Setter
+@Getter
 @Document(collection = "salesAndTrafficReports")
 public class SalesAndTrafficReport {
 
@@ -20,44 +20,5 @@ public class SalesAndTrafficReport {
     private List<SalesAndTrafficByDate> salesAndTrafficByDate;
     private List<SalesAndTrafficByAsin> salesAndTrafficByAsin;
 
-    public SalesAndTrafficReport(UUID id, ReportSpecification reportSpecification, List<SalesAndTrafficByDate> salesAndTrafficByDate, List<SalesAndTrafficByAsin> salesAndTrafficByAsin) {
-
-        this.id = id;
-        this.reportSpecification = reportSpecification;
-        this.salesAndTrafficByDate = salesAndTrafficByDate;
-        this.salesAndTrafficByAsin = salesAndTrafficByAsin;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public ReportSpecification getReportSpecification() {
-        return reportSpecification;
-    }
-
-    public void setReportSpecification(ReportSpecification reportSpecification) {
-        this.reportSpecification = reportSpecification;
-    }
-
-    public List<SalesAndTrafficByDate> getSalesAndTrafficByDate() {
-        return salesAndTrafficByDate;
-    }
-
-    public void setSalesAndTrafficByDate(List<SalesAndTrafficByDate> salesAndTrafficByDate) {
-        this.salesAndTrafficByDate = salesAndTrafficByDate;
-    }
-
-    public List<SalesAndTrafficByAsin> getSalesAndTrafficByAsin() {
-        return salesAndTrafficByAsin;
-    }
-
-    public void setSalesAndTrafficByAsin(List<SalesAndTrafficByAsin> salesAndTrafficByAsin) {
-        this.salesAndTrafficByAsin = salesAndTrafficByAsin;
-    }
 }
 
