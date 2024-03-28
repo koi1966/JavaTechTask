3f1d24323b1f9b71f3475f485871c498ac71f7a6
Oleg Klimchuk <koi196621@gmail.com>
2024-03-28 17:41:02 +0200

    Getting started with a project using MongoDB.  Added use of repository.  Added entity class java Trying import data to mongodb from json file using java 28.03.2024

--- src/main/java/com/example/javatechtask/models/repository/TrafficByDateRepository.java
+++ src/main/java/com/example/javatechtask/models/repository/TrafficByDateRepository.java
@@ -1,14 +1,17 @@
 package com.example.javatechtask.models.repository;
 
 import com.example.javatechtask.models.SalesAndTrafficByDate;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 @EnableMongoRepositories
+@Repository
 public interface TrafficByDateRepository extends MongoRepository<SalesAndTrafficByDate, String> {
 //    @Query("{ 'date' : ?0 }")
     List<SalesAndTrafficByDate> findByDate(String date);
@@ -18,6 +21,10 @@ public interface TrafficByDateRepository extends MongoRepository<SalesAndTraffic
 
 //    https://www.baeldung.com/queries-in-spring-data-mongodb
 
+    @Aggregation(pipeline = {
+            "{ $group: { _id: \"$date\", totalSales: { $sum: \"$salesByDate.unitsOrdered\" }, totalTraffic: { $sum: \"$trafficByDate.visitors\" } } }"
+    })
+    List<Object> aggregateSalesAndTraffic();
 
 }
 
